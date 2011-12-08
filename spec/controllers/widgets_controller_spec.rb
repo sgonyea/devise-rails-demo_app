73576d2138f5ee594a4aabf05f5ea68faec8ce8a
Scott Gonyea <sgonyea@truecar.com>
2011-12-08 15:44:31 -0800

    Include the devise helpers for testing

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -19,6 +19,7 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe WidgetsController do
+  include Devise::TestHelpers
 
   # This should return the minimal set of attributes required to create a valid
   # Widget. As you add validations to Widget, be sure to
