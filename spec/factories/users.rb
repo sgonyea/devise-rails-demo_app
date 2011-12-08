849a5944533da621c0342344fda9cf9737acee7f
Scott Gonyea <sgonyea@truecar.com>
2011-12-08 15:54:27 -0800

    Added password / confirmed_at to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,10 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "foo_#{n}@bar.com" }
 
-    name      "FooBar McBaz"
-    is_admin  false
+    name          "FooBar McBaz"
+    password      'secret23'
+    confirmed_at  Time.now
+    is_admin      false
 
     factory :admin_user do
       is_admin true
