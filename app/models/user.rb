1711073768c2747384e07229c0dff2200f175cf5
Scott Gonyea <sgonyea@truecar.com>
2011-12-08 15:09:32 -0800

    registerable adds more path helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :confirmable, :recoverable,
+  devise :database_authenticatable, :confirmable, :recoverable, :registerable,
          :rememberable, :lockable, :trackable, :token_authenticatable
 
   attr_accessible :email, :password, :password_confirmation
